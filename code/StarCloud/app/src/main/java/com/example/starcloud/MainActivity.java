265fde81e64fe3f9060231e60b41a5a0b6c102a9
wxli <wxli@grandstream.cn>
2022-08-21 23:26:48 +0800

    file list

--- code/StarCloud/app/src/main/java/com/example/starcloud/MainActivity.java
+++ code/StarCloud/app/src/main/java/com/example/starcloud/MainActivity.java
@@ -2,35 +2,19 @@ package com.example.starcloud;
 
 import android.Manifest;
 import android.content.Intent;
-import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
-
-import com.google.android.material.snackbar.Snackbar;
+import android.os.Environment;
+import android.provider.DocumentsContract;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.os.Environment;
-import android.provider.DocumentsContract;
-import android.util.Log;
-import android.view.View;
-
 import androidx.core.app.ActivityCompat;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
 
 import com.example.starcloud.databinding.ActivityMainBinding;
-
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.ViewGroup;
-import android.widget.LinearLayout;
-import android.widget.RelativeLayout;
-import android.widget.TextView;
-import android.widget.Toast;
 
 import java.io.BufferedReader;
 import java.io.File;
@@ -42,7 +26,7 @@ import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.Socket;
 import java.net.URL;
-import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -51,9 +35,13 @@ public class MainActivity extends AppCompatActivity {
     int EXIST = 1;
     int FAIL = 2;
     int retCode = 0;
+    String WEBURL = "http://5db2289.r3.cpolar.top";
     String retStr = "";
-    String rootPath = null;
+    String ROOTPATH = null;
+    String SYSPATH = null;
     Socket cliSocket = null;
+    ListView fileListView = null;
+    ArrayList<String> fileList = new ArrayList<String>();
     private ActivityMainBinding binding;
 
     @Override
@@ -62,15 +50,16 @@ public class MainActivity extends AppCompatActivity {
 
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-        setSupportActionBar(binding.toolbar);
         ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 222);
 
-        rootPath = Environment.getExternalStorageDirectory().getAbsolutePath()+"/StarCloud/";
+        ROOTPATH = Environment.getExternalStorageDirectory().getAbsolutePath()+"/StarCloud/";
+        SYSPATH = ROOTPATH+"sys/";
         binding.fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Uri uri = Uri.parse("content://com.android.externalstorage.documents/document/primary:%2fStarCloud%2f");
-                new File(rootPath).mkdir();
+                new File(ROOTPATH).mkdir();
+                new File(SYSPATH).mkdir();
 
                 httpDownloadThread th = new httpDownloadThread();
                 retStr = "";
@@ -83,6 +72,9 @@ public class MainActivity extends AppCompatActivity {
                 startActivityForResult(localIntent, FILE_REQ_CODE);
             }
         });
+
+        fileListView = (ListView)findViewById(R.id.FileListView);
+        new httpListThread().start();
     }
 
     @Override
@@ -113,13 +105,70 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    class  httpListThread extends Thread {
+        @Override
+        public void run() {
+            super.run();
+
+            HttpURLConnection conn = null;
+            InputStream input = null;
+            String content = "";
+
+            try {
+                System.out.println("==========1====");
+                URL url = new URL(WEBURL);
+                conn = (HttpURLConnection)url.openConnection();
+                input = conn.getInputStream();
+                InputStreamReader ir = new InputStreamReader(input);
+                BufferedReader reader = new BufferedReader(ir);
+
+                String line = null;
+                while((line = reader.readLine()) != null) {
+                    content = content + line + "\n";
+                }
+                System.out.println("========2======");
+                fileList.clear();
+                while (true)
+                {
+                    int size1 = content.indexOf("<a href=\"");
+                    int size2 = content.indexOf("</a>");
+                    if (size1 < 0|| size2 < 0) {
+                        break;
+                    }
+                    String tmp = content.substring(size1 + 9, size2);
+                    content = content.substring(size2 + 4);
+                    fileList.add(tmp.substring(tmp.indexOf(">") + 1));
+                    System.out.println(tmp+"==\n");
+                }
+
+                for (int i = fileList.size(); i <= 0; i++) {
+                    System.out.println(fileList.get(i)+"\n");
+                }
+
+                ArrayAdapter<String> adapter = new ArrayAdapter<>
+                        (MainActivity.this, android.R.layout.simple_list_item_1, fileList);
+                fileListView.setAdapter(adapter);
+            } catch (Exception e) {
+                e.printStackTrace();
+            } finally {
+                try {
+                    input.close();
+                    conn.disconnect();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+
     class  httpDownloadThread extends Thread {
         @Override
         public void run() {
             super.run();
 
-            String urlStr="http://19b0ae1c.vip.cpolar.cn/test.txt";
-            String fileName = rootPath + "test.txt";
+            String urlStr=WEBURL+"/test.txt";
+            String fileName = ROOTPATH + "test.txt";
             OutputStream output = null;
             URL url = null;
             HttpURLConnection conn = null;
@@ -130,7 +179,6 @@ public class MainActivity extends AppCompatActivity {
                 url = new URL(urlStr);
                 conn = (HttpURLConnection)url.openConnection();
                 input = conn.getInputStream();
-                new File(rootPath).mkdir();
                 file = new File(fileName);
                 if (file.exists())  {
                     retStr = "文件已存在" + fileName;
@@ -166,6 +214,7 @@ public class MainActivity extends AppCompatActivity {
             }
         }
     }
+
     class cliThread extends Thread {
         @Override
         public void run() {
